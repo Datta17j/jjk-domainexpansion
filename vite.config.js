c517b3a9db5646a9df9d3e9dbc80c88a0a5d1007
Datta17j <debdatta17j@gmail.com>
2026-06-29 20:15:27 +0530

    railway fixes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: '/',
+  preview: {
+    allowedHosts: ['jjk-domainexpansion-production.up.railway.app'],
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+  }
 })
